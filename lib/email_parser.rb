d41906461e46e8ab68f1fc3f2b56c9180252bece
Emily Jirles <emily.jirles@gmail.com>
2018-05-01 12:42:05 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,11 +11,12 @@ class EmailParser
   end
   
   def parse
-    if @emails.include?(',')
-      email_list = @emails.split(', ')
-    else
-      email_list = @emails.split(' ')
-    end
+    #if @emails.include?(',')
+    email_list = @emails.split(', ')
+    #else
+    email_list.each{
+    @emails.split(' ')
+    #end
     email_list.uniq
   end
   
